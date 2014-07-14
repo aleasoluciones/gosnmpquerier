33968ddbdb9b48a2396a7c8a486f3bf89c9295f4
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2014-07-14 10:49:45 +0200

    Refactor (to clarify the flow)

--- snmpqueries.go
+++ snmpqueries.go
@@ -9,37 +9,39 @@ import (
 	"github.com/eferro/go-snmpqueries/pkg/snmpquery"
 )
 
-func generateRandomQueries() <-chan snmpquery.Query {
-	out := make(chan snmpquery.Query)
-	go func() {
-		queryId := 0
-		for {
-			query := snmpquery.Query{
-				Id:          queryId,
-				Query:       "Fake query " + strconv.Itoa(queryId),
-				Destination: "Fake destination " + strconv.Itoa(queryId),
-			}
-			out <- query
-			queryId += 1
-			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+func generateRandomQueries(input chan snmpquery.Query) {
+	queryId := 0
+	for {
+		query := snmpquery.Query{
+			Id:          queryId,
+			Query:       "Fake query " + strconv.Itoa(queryId),
+			Destination: "Fake destination " + strconv.Itoa(queryId),
 		}
-	}()
-	return out
+		input <- query
+		queryId += 1
+		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+	}
 }
 
-func main() {
+func processQueries(input chan snmpquery.Query, processed chan snmpquery.Query) {
+	for query := range input {
+		snmpquery.HandleQuery(&query)
+		processed <- query
+	}
+}
 
-	input := generateRandomQueries()
-
-	processed := make(chan snmpquery.Query)
-	go func() {
-		for query := range input {
-			snmpquery.HandleQuery(&query)
-			processed <- query
-		}
-	}()
-
+func printResults(processed chan snmpquery.Query) {
 	for query := range processed {
 		fmt.Println(query.Query, query.Response)
 	}
+}
+
+func main() {
+	input := make(chan snmpquery.Query)
+	processed := make(chan snmpquery.Query)
+
+	go generateRandomQueries(input)
+	go processQueries(input, processed)
+
+	printResults(processed)
 }
