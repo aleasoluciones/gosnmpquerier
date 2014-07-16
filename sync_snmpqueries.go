b22507deda0d35a014beb1bd8308c91954b5bfd0
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2014-07-16 13:41:48 +0200

    Changed default example

--- sync_snmpqueries.go
+++ sync_snmpqueries.go
@@ -10,14 +10,14 @@ const (
 	CONTENTION = 4
 )
 
-
 func main() {
 	var input = make(chan snmpquery.QueryWithOutputChannel)
 	go snmpquery.ProcessAndDispatchQueries(input, CONTENTION)
 
 	for id := 0; id < 10; id++ {
-		q, _ := snmpquery.FromJson(`{"command":"walk", "destination":"localhost", "community":"kaleidos", "oid":"1.3.6.1.2.1.2.2"}`)
+		q, _ := snmpquery.FromJson(`{"command":"walk", "destination":"localhost", "community":"public", "oid":"1.3.6.1"}`)
 		q.Id = id
+
 		fmt.Println("Result:", snmpquery.ExecuteQuery(input, *q))
 	}
 }
