549ca8f58a08202cec663a21aa2d6c8227a6155f
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2014-07-16 10:59:34 +0200

    json related coded extracted

--- snmpqueries.go
+++ snmpqueries.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/eferro/go-snmpqueries/pkg/snmpquery"
 )
@@ -13,16 +11,6 @@ import (
 const (
 	CONTENTION = 4
 )
-
-type QueryMessage struct {
-	Command        string
-	Destination    string
-	Community      string
-	Oid            string
-	Timeout        int
-	Retries        int
-	AdditionalInfo interface{}
-}
 
 func readLinesFromStdin(inputLines chan string) {
 	reader := bufio.NewReader(os.Stdin)
@@ -43,40 +31,10 @@ func readQueriesFromStdin(input chan snmpquery.Query) {
 
 	queryId := 0
 	for line := range inputLines {
-
-		var m QueryMessage
-		m.Timeout = 2
-		m.Retries = 1
-
-		b := []byte(line)
-		err := json.Unmarshal(b, &m)
-		if err != nil {
-			fmt.Println("Invalid line format", err, line)
-		}
-
-		cmd, err := convertCommand(m.Command)
-		query := snmpquery.Query{
-			Id:          queryId,
-			Cmd:         cmd,
-			Community:   m.Community,
-			Oid:         m.Oid,
-			Destination: m.Destination,
-			Timeout:     time.Duration(m.Timeout) * time.Second,
-			Retries:     m.Retries,
-		}
-		input <- query
+		query := snmpquery.FromJson(line)
+		query.Id = queryId
+		input <- *query
 		queryId += 1
-	}
-}
-
-func convertCommand(command string) (snmpquery.OpSnmp, error) {
-	switch command {
-	case "walk":
-		return snmpquery.WALK, nil
-	case "get":
-		return snmpquery.GET, nil
-	default:
-		return 0, fmt.Errorf("Unsupported command %s ", command)
 	}
 }
 
