2faefe41c80af3ba9c88c72964cae5612348c7c2
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2014-07-16 12:22:04 +0200

    Publising to amqp exchange

---
+++ amqpsnmpqueries.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/eferro/go-snmpqueries/pkg/amqp"
+	"github.com/eferro/go-snmpqueries/pkg/snmpquery"
+)
+
+const (
+	CONTENTION = 4
+)
+
+type QueryMessage struct {
+	Command        string
+	Destination    string
+	Community      string
+	Oid            string
+	Timeout        int
+	Retries        int
+	AdditionalInfo interface{}
+}
+
+func readLinesFromStdin(inputLines chan string) {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			close(inputLines)
+			return
+		}
+		inputLines <- line
+	}
+}
+
+func readQueriesFromStdin(input chan snmpquery.Query) {
+
+	inputLines := make(chan string, 10)
+	go readLinesFromStdin(inputLines)
+
+	queryId := 0
+	for line := range inputLines {
+
+		var m QueryMessage
+		m.Timeout = 2
+		m.Retries = 1
+
+		b := []byte(line)
+		err := json.Unmarshal(b, &m)
+		if err != nil {
+			fmt.Println("Invalid line format", err, line)
+		}
+
+		cmd, err := convertCommand(m.Command)
+		query := snmpquery.Query{
+			Id:          queryId,
+			Cmd:         cmd,
+			Community:   m.Community,
+			Oid:         m.Oid,
+			Destination: m.Destination,
+			Timeout:     time.Duration(m.Timeout) * time.Second,
+			Retries:     m.Retries,
+		}
+		input <- query
+		queryId += 1
+	}
+}
+
+func convertCommand(command string) (snmpquery.OpSnmp, error) {
+	switch command {
+	case "walk":
+		return snmpquery.WALK, nil
+	case "get":
+		return snmpquery.GET, nil
+	default:
+		return 0, fmt.Errorf("Unsupported command %s ", command)
+	}
+}
+
+func publishResults(processed chan snmpquery.Query, amqpUri, exchangeName, routingKey string) {
+	for query := range processed {
+		jsonText, err := snmpquery.ToJson(&query)
+		if err != nil {
+			fmt.Println("Error converting to json:", err, query)
+			continue
+		}
+		amqp.Publish(amqpUri, exchangeName, "direct", routingKey, jsonText, false)
+	}
+}
+
+func main() {
+
+	amqpUri := flag.String("amqp_uri", "amqp://guest:guest@localhost:5672/", "AMQP uri")
+	exchangeName := flag.String("exchange", "test-exchange", "Durable AMQP exchange name")
+	routingKey := flag.String("key", "", "AMQP routing key")
+	flag.Parse()
+
+	fmt.Println(*amqpUri, *exchangeName, *routingKey)
+
+	input := make(chan snmpquery.Query, 10)
+	processed := make(chan snmpquery.Query, 10)
+
+	go readQueriesFromStdin(input)
+	go snmpquery.Process(input, processed, CONTENTION)
+
+	publishResults(processed, *amqpUri, *exchangeName, *routingKey)
+}
---
+++ pkg/amqp/amqp.go
@@ -0,0 +1,89 @@
+package amqp
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func Publish(amqpURI, exchange, exchangeType, routingKey, body string, reliable bool) error {
+
+	// This function dials, connects, declares, publishes, and tears down,
+	// all in one go. In a real service, you probably want to maintain a
+	// long-lived connection as state, and publish against that.
+
+	log.Printf("dialing %q", amqpURI)
+	connection, err := amqp.Dial(amqpURI)
+	if err != nil {
+		return fmt.Errorf("Dial: %s", err)
+	}
+	defer connection.Close()
+
+	log.Printf("got Connection, getting Channel")
+	channel, err := connection.Channel()
+	if err != nil {
+		return fmt.Errorf("Channel: %s", err)
+	}
+
+	log.Printf("got Channel, declaring %q Exchange (%q)", exchangeType, exchange)
+	if err := channel.ExchangeDeclare(
+		exchange,     // name
+		exchangeType, // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // noWait
+		nil,          // arguments
+	); err != nil {
+		return fmt.Errorf("Exchange Declare: %s", err)
+	}
+
+	// Reliable publisher confirms require confirm.select support from the
+	// connection.
+	if reliable {
+		log.Printf("enabling publishing confirms.")
+		if err := channel.Confirm(false); err != nil {
+			return fmt.Errorf("Channel could not be put into confirm mode: %s", err)
+		}
+
+		ack, nack := channel.NotifyConfirm(make(chan uint64, 1), make(chan uint64, 1))
+
+		defer confirmOne(ack, nack)
+	}
+
+	log.Printf("declared Exchange, publishing %dB body (%q)", len(body), body)
+	if err = channel.Publish(
+		exchange,   // publish to an exchange
+		routingKey, // routing to 0 or more queues
+		false,      // mandatory
+		false,      // immediate
+		amqp.Publishing{
+			Headers:         amqp.Table{},
+			ContentType:     "text/plain",
+			ContentEncoding: "",
+			Body:            []byte(body),
+			DeliveryMode:    amqp.Transient, // 1=non-persistent, 2=persistent
+			Priority:        0,              // 0-9
+			// a bunch of application/implementation-specific fields
+		},
+	); err != nil {
+		return fmt.Errorf("Exchange Publish: %s", err)
+	}
+
+	return nil
+}
+
+// One would typically keep a channel of publishings, a sequence number, and a
+// set of unacknowledged sequence numbers and loop until the publishing channel
+// is closed.
+func confirmOne(ack, nack chan uint64) {
+	log.Printf("waiting for confirmation of one publishing")
+
+	select {
+	case tag := <-ack:
+		log.Printf("confirmed delivery with delivery tag: %d", tag)
+	case tag := <-nack:
+		log.Printf("failed delivery of delivery tag: %d", tag)
+	}
+}
