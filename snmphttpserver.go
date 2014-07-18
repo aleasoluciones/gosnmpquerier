f67ac79a34c678dca1c76e633f9923566c3cdd43
Yamila Moreno <yamila.ms@gmail.com>
2014-07-18 09:48:27 +0200

    Fixed formatting in go style (with tabs >_<)

--- snmphttpserver.go
+++ snmphttpserver.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "time"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 
 	"github.com/eferro/go-snmpqueries/pkg/snmpquery"
 )
 
 const (
-    CONTENTION = 4
+	CONTENTION = 4
 )
 
 func rootHandler(input chan snmpquery.QueryWithOutputChannel, w http.ResponseWriter, r *http.Request) {
 
-    cmd, _ := snmpquery.ConvertCommand(r.FormValue("cmd"))
-    query := snmpquery.Query{
-        Cmd:         cmd,
-        Community:   r.FormValue("community"),
-        Oid:         r.FormValue("oid"),
-        Timeout:     time.Duration(10) * time.Second,
-        Retries:     1,
-        Destination: r.FormValue("destination"),
-    }
-    processed := snmpquery.ExecuteQuery(input, query)
-    jsonProcessed, err := snmpquery.ToJson(&processed)
-    if err != nil {
-        fmt.Fprint(w, err)
-    }
-    fmt.Fprint(w, jsonProcessed)
+	cmd, _ := snmpquery.ConvertCommand(r.FormValue("cmd"))
+	query := snmpquery.Query{
+		Cmd:         cmd,
+		Community:   r.FormValue("community"),
+		Oid:         r.FormValue("oid"),
+		Timeout:     time.Duration(10) * time.Second,
+		Retries:     1,
+		Destination: r.FormValue("destination"),
+	}
+	processed := snmpquery.ExecuteQuery(input, query)
+	jsonProcessed, err := snmpquery.ToJson(&processed)
+	if err != nil {
+		fmt.Fprint(w, err)
+	}
+	fmt.Fprint(w, jsonProcessed)
 
 }
 
 func main() {
-    var input = make(chan snmpquery.QueryWithOutputChannel)
-    go snmpquery.ProcessAndDispatchQueries(input, CONTENTION)
+	var input = make(chan snmpquery.QueryWithOutputChannel)
+	go snmpquery.ProcessAndDispatchQueries(input, CONTENTION)
 
-    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        rootHandler(input, w, r)
-    })
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rootHandler(input, w, r)
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
