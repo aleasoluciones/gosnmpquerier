23953bf934c544605a62509f1220db7b313e3753
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2014-07-23 23:04:04 +0200

    Refactor and restructuring code

---
+++ pkg/snmpquery/syncquerier.go
@@ -0,0 +1,40 @@
+package snmpquery
+
+type SyncQuerier struct {
+	Input        chan QueryWithOutputChannel
+	asyncQuerier *AsyncQuerier
+}
+
+func NewSyncQuerier(contention int) *SyncQuerier {
+	querier := SyncQuerier{
+		Input:        make(chan QueryWithOutputChannel),
+		asyncQuerier: NewAsyncQuerier(contention),
+	}
+	go querier.processAndDispatchQueries()
+	return &querier
+}
+
+func (querier *SyncQuerier) ExecuteQuery(query Query) Query {
+	output := make(chan Query)
+	querier.Input <- QueryWithOutputChannel{query, output}
+	processedQuery := <-output
+	return processedQuery
+}
+
+func (querier *SyncQuerier) processAndDispatchQueries() {
+
+	m := make(map[int]chan Query)
+	i := 0
+	for {
+		select {
+		case queryWithOutputChannel := <-querier.Input:
+			queryWithOutputChannel.query.Id = i
+			i += 1
+			m[queryWithOutputChannel.query.Id] = queryWithOutputChannel.responseChannel
+			querier.asyncQuerier.Input <- queryWithOutputChannel.query
+		case processedQuery := <-querier.asyncQuerier.Output:
+			m[processedQuery.Id] <- processedQuery
+			delete(m, processedQuery.Id)
+		}
+	}
+}
