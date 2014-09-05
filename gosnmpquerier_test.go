6b5b7b88550773e537656afa35d96bc0b796b146
Guillermo Pascual <akira.space@gmail.com>
2014-09-05 13:56:41 +0200

    Refactor tests

--- gosnmpquerier_test.go
+++ gosnmpquerier_test.go
@@ -11,16 +11,19 @@ import (
 type FakeSnmpClient struct{}
 
 func (snmpClient *FakeSnmpClient) get(destination, community string, oids []string, timeout time.Duration, retries int) ([]gosnmp.SnmpPDU, error) {
-	return []gosnmp.SnmpPDU{gosnmp.SnmpPDU{Name: "foo", Type: 1, Value: 1}}, nil
+	return makeSnmpPDu()
 }
 
 func (snmpClient *FakeSnmpClient) walk(destination, community, oid string, timeout time.Duration, retries int) ([]gosnmp.SnmpPDU, error) {
-	return []gosnmp.SnmpPDU{gosnmp.SnmpPDU{Name: "foo", Type: 1, Value: 1}}, nil
+	return makeSnmpPDu()
 }
 
 func (snmpClient *FakeSnmpClient) getnext(destination, community string, oids []string, timeout time.Duration, retries int) ([]gosnmp.SnmpPDU, error) {
-	return []gosnmp.SnmpPDU{gosnmp.SnmpPDU{Name: "foo", Type: 1, Value: 1}}, nil
+	return makeSnmpPDu()
+}
 
+func makeSnmpPDu() ([]gosnmp.SnmpPDU, error) {
+	return []gosnmp.SnmpPDU{gosnmp.SnmpPDU{Name: "foo", Type: 1, Value: 1}}, nil
 }
 
 func syncQuerier() *SyncQuerier {
@@ -29,20 +32,23 @@ func syncQuerier() *SyncQuerier {
 	return querier
 }
 
+func expectedSnmpResult() []gosnmp.SnmpPDU {
+	return []gosnmp.SnmpPDU{gosnmp.SnmpPDU{Name: "foo", Type: 0x1, Value: 1}}
+}
 func TestGetReturnsSnmpGetResult(t *testing.T) {
 	querier := syncQuerier()
 	result, _ := querier.Get("192.168.5.15", "alea2", []string{"1.3.6.1.2.1.1.1.0"}, 1*time.Second, 1)
-	assert.Equal(t, result, []gosnmp.SnmpPDU{gosnmp.SnmpPDU{Name: "foo", Type: 0x1, Value: 1}})
+	assert.Equal(t, result, expectedSnmpResult())
 }
 
 func TestGetNextReturnsSnmpGetNextResult(t *testing.T) {
 	querier := syncQuerier()
 	result, _ := querier.GetNext("192.168.5.15", "alea2", []string{"1.3.6.1.2.1.1.1.0"}, 1*time.Second, 1)
-	assert.Equal(t, result, []gosnmp.SnmpPDU{gosnmp.SnmpPDU{Name: "foo", Type: 0x1, Value: 1}})
+	assert.Equal(t, result, expectedSnmpResult())
 }
 
 func TestWalkReturnsSnmpWalkResult(t *testing.T) {
 	querier := syncQuerier()
 	result, _ := querier.Walk("192.168.5.15", "alea2", "1.3.6.1.2.1.1", 1*time.Second, 1)
-	assert.Equal(t, result, []gosnmp.SnmpPDU{gosnmp.SnmpPDU{Name: "foo", Type: 0x1, Value: 1}})
+	assert.Equal(t, result, expectedSnmpResult())
 }
