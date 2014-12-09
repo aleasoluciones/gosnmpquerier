ec22f2f32fe8840cf65fa467c5f3e18e4a8c89b1
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2014-12-09 18:54:59 +0100

    Improved logging

--- asyncquerier.go
+++ asyncquerier.go
@@ -132,7 +132,7 @@ func (processor *destinationProcessor) processQueriesFromChannel(processorId str
 		select {
 		case query, more := <-processor.input:
 			if more {
-				log.Println("Processing query for ", query.Destination, " pending ", more)
+				log.Println("Processing query for ", query.Destination, " pending ", len(processor.input))
 				processor.handleQuery(&query)
 				processor.output <- query
 			} else {
