76a4985b93d1b89e0cc7f8a5874f7bc5a244b356
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2015-01-15 15:50:51 +0100

    Support for multiple oids get

--- examples/get.go
+++ examples/get.go
@@ -3,18 +3,29 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/aleasoluciones/gosnmpquerier"
 )
 
 func main() {
-	community := flag.String("community", "alea2", "snmp v2 community")
-	host := flag.String("host", "192.168.5.15", "host")
-	oid := flag.String("oid", "1.3.6.1.2.1.1.1.0", "Oid to get")
+	community := flag.String("community", "puiblic", "snmp v2 community")
+	host := flag.String("host", "127.0.0.1", "host")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s: [options] [[oid] ...]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
 	flag.Parse()
 
+	oids := []string{}
+	for _, oid := range flag.Args() {
+		oids = append(oids, oid)
+	}
+
 	querier := gosnmpquerier.NewSyncQuerier(1, 3, 3*time.Second)
-	result, err := querier.Get(*host, *community, []string{*oid}, 1*time.Second, 1)
+	result, err := querier.Get(*host, *community, oids, 1*time.Second, 1)
 	fmt.Println(result, err)
 }
